7934d20b4fe4e0b4dc72c044b209184470e2e3da
mkurleto <michal.kurleto@gmail.com>
2023-09-17 13:26:37 +0200

    add middleware chaining with alice

--- cmd/web/routers.go
+++ cmd/web/routers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
@@ -12,5 +15,8 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	// standard middleware for all requests
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 }
