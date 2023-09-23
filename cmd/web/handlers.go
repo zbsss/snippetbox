671be032b745351bfa5bbd6edb9a737795097e92
mkurleto <michal.kurleto@gmail.com>
2023-09-23 18:10:19 +0200

    fix form decoding bug

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -71,7 +71,7 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	var form snippetCreateForm
 
-	err := app.decodePostForm(r, form)
+	err := app.decodePostForm(r, &form)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
